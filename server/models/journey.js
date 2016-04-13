c565b86a39a2c125ffd014a5e03f243f3fa1a519
munazir <munazirahsan@gmail.com>
2016-04-13 22:05:12 +0530

    initial commit

---
+++ server/models/journey.js
@@ -0,0 +1,40 @@
+/**
+ * Created by Pratyush on 29-03-2016.
+ */
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var JourneySchema = new Schema({
+    posted_by: {type: String, required: true, ref: 'User'},
+    start: {
+        street: {type: String, required: true},
+        area: {type: String, required: true},
+        //city: {type: Schema.ObjectId, required: true, ref: 'City'}
+    },
+    end: {
+        street: {type: String, required: true},
+        area: {type: String, required: true},
+        //city: {type: Schema.ObjectId, required: true, ref: 'City'}
+    },
+    departure: {type: Date, required: true},
+    vehicle: {type: String, required: true},
+    availableSeats: {type: Number, required: true, min: 1, max: 5},
+    genderPreference: {type: String, default: 'none'},
+    stops: [String],
+    description: String,
+    fare: {type: Number, required: true},
+    requested_by: [{
+        name: {type: String},
+        requestTime: {type: Date, default: Date.now()},
+    }],
+    accepted_requests: [{
+        name: {type: String},
+        acceptedTime: {type: Date, default: Date.now()},
+        //city: {type: Schema.ObjectId, required: true, ref: 'City'}
+    }],
+    created_at: {type: Date, default: Date.now()}
+});
+
+
+module.exports = mongoose.model('Journey', JourneySchema);
